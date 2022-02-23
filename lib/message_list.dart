60d62673420bc925ad1381268bc0a20378fff883
fischerliste <fischerliste@gmail.com>
2022-02-23 12:41:15 +0100

    Showing the Dialog WORKS NOW

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,15 +1,9 @@
-//import 'dart:ffi';
-
-// import 'message_widget.dart';
 import 'package:flutter/material.dart';
-// import 'data/message.dart';
+import 'package:flutter/scheduler.dart';
 import 'data/message_dao.dart';
-// import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MessageListState extends State<MessageList> {
-  // final TextEditingController _messageController = TextEditingController();
-  // final ScrollController _scrollController = ScrollController();
   String? uid;
   SharedPreferences? prefs;
 
@@ -22,49 +16,15 @@ class MessageListState extends State<MessageList> {
     super.initState();
   }
 
-  signupDialog() {
-    return "UID";
-  }
-
-  signinDialog() {
-    bool checkSignin() {
-      return true;
-    }
-
-    return AlertDialog(
-      title: const Text("Please sign in!"),
-      content: const Icon(Icons.account_box_outlined),
-      actions: <Widget>[
-        TextButton(
-            onPressed: () {
-              String id = signupDialog();
-              Navigator.pop(context, id);
-            },
-            child: const Text("Registrieren")),
-        TextButton(
-            onPressed: () {
-              if (checkSignin()) {
-                Navigator.pop(context);
-              }
-            },
-            child: const Text("Anmelden")),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-
-
-    //WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToBottom());
     if (uid == null) {
-      uid = showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (context) {
-            return signinDialog();
-          }) as String;
-      prefs!.setString('UID', uid!);
+      SchedulerBinding.instance?.addPostFrameCallback((_) {
+        Navigator.of(context).push(showSigninPopup(context)).then((value) {
+          prefs!.setString('UID', uid!);
+          return uid = value;
+        });
+      });
     }
 
     ///@TODO: Add Navigator fix for the dialog!
@@ -139,37 +99,41 @@ class MessageListState extends State<MessageList> {
           child: Icon(Icons.add_to_queue_sharp),
         ));
   }
+}
 
-  // void _sendMessage() {
-  //   if (_canSendMessage()) {
-  //     final message = Message(_messageController.text, DateTime.now());
-  //     widget.messageDao.saveMessage(message);
-  //     _messageController.clear();
-  //     setState(() {});
-  //   }
-  // }
+DialogRoute showSigninPopup(BuildContext buildcontext) {
+  signupDialog() {
+    return "UID";
+  }
 
-  // Widget _getMessageList() {
-  //   return Expanded(
-  //     child: FirebaseAnimatedList(
-  //       controller: _scrollController,
-  //       query: widget.messageDao.getMessageQuery(),
-  //       itemBuilder: (context, snapshot, animation, index) {
-  //         final json = snapshot.value as Map<dynamic, dynamic>;
-  //         final message = Message.fromJson(json);
-  //         return MessageWidget(message.text, message.date);
-  //       },
-  //     ),
-  //   );
-  // }
+  bool checkSignin() {
+    return true;
+  }
 
-  // bool _canSendMessage() => _messageController.text.isNotEmpty;
-
-  // void _scrollToBottom() {
-  //   if (_scrollController.hasClients) {
-  //     _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
-  //   }
-  // }
+  return DialogRoute(
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Please sign in!"),
+        content: const Icon(Icons.account_box_outlined),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {
+                String id = signupDialog();
+                Navigator.pop(context, id);
+              },
+              child: const Text("Registrieren")),
+          TextButton(
+              onPressed: () {
+                if (checkSignin()) {
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text("Anmelden")),
+        ],
+      );
+    },
+    context: buildcontext,
+  );
 }
 
 class MessageList extends StatefulWidget {
